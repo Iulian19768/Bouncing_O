bb0649cddfca14f4305811dd7b29c1d508d085b8
Iulian Matei <iulianmatei19768@gmail.com>
2023-04-18 21:46:39 +0300

    Bouncing Ball
    A simple bouncing "O".

---
+++ animatione/animatione/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace animatione
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Ball = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Ball
+            // 
+            this.Ball.AutoSize = true;
+            this.Ball.Font = new System.Drawing.Font("Javanese Text", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ball.Location = new System.Drawing.Point(28, 36);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(175, 272);
+            this.Ball.TabIndex = 0;
+            this.Ball.Text = "o";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1086, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Schimba culoarea";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1256, 740);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Ball);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label Ball;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ animatione/animatione/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace animatione
+{
+    public partial class Form1 : Form
+    {
+        private int ballWidth = 300;
+        private int ballHeight = 300;
+        private int ballPosX = 0;
+        private int ballPosY = 0;
+        private int moveStepX = 4;
+        private int moveStepY = 4;
+        int milliseconds = 50;
+        int BoundaryX = 727;
+        int BoundaryY = 478;
+        int vx = 1, vy = 1;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            if (Ball.Left > BoundaryX || Ball.Left <= 0)
+            {
+                vx *= -2; 
+                // negative changes direction
+            }
+
+            if (Ball.Top <= 0)
+            {
+                vy *= -1;
+            }
+
+            if (Ball.Top > BoundaryY)
+            {
+                vy *= -1;
+                vy += 0;        // after coliision with ground maximum height decreases each time
+            }
+
+            if (Ball.Left > BoundaryX)
+            {
+                vx *= 0;
+                vx += -1;        // after coliision with ground maximum height decreases each time
+            }
+            vy++;               // ball is always attracted by gravity
+
+            if (Ball.Top > BoundaryY) Ball.Top = BoundaryY;
+
+
+            Ball.Left = Ball.Left + vx;
+            Ball.Top = Ball.Top + vy;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            cd.AllowFullOpen = true;
+            cd.FullOpen = true;
+            cd.AnyColor = true;
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                Ball.ForeColor = cd.Color;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+
+        }
+
+
+    }
+
+}
+
+
